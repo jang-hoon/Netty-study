b800168f92c645c3058693555bf2a91678753cdf
hoonjang <hoonjang@ex-em.com>
2017-01-19 21:46:45 +0900

    add Example1_4_basic, Example1_4_edited

---
+++ projects/netty-in-action/src/main/java/kr/lunawyrd/nettypractice/lecture1/Example1_4_basic.java
@@ -0,0 +1,34 @@
+package kr.lunawyrd.nettypractice.lecture1;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Example1_4_basic {
+	public static void main(String[] args) {
+		Channel channel = new NioSocketChannel();
+		new NioEventLoopGroup().register(channel);
+		ChannelFuture future = channel.connect(new InetSocketAddress("127.0.0.1", 7777));
+		future.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) throws Exception {
+				if(future.isSuccess()){
+					ByteBuf buffer = Unpooled.copiedBuffer("Hello", Charset.defaultCharset());
+					ChannelFuture wf = future.channel().writeAndFlush(buffer);
+				}
+				else{
+					Throwable cause = future.cause();
+					cause.printStackTrace();
+				}
+			}
+		});
+	}
+	
+}
---
+++ projects/netty-in-action/src/main/java/kr/lunawyrd/nettypractice/lecture1/Example1_4_edited.java
@@ -0,0 +1,47 @@
+package kr.lunawyrd.nettypractice.lecture1;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Example1_4_edited {
+	
+	public static void main(String[] args) {
+		Channel channel = new NioSocketChannel();
+		new NioEventLoopGroup().register(channel);
+		
+		ChannelFuture future = channel.connect(new InetSocketAddress("127.0.0.1", 7777));
+		future.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) throws Exception {
+				if(future.isSuccess()){
+					System.out.println("connected writeable=" + future.channel().isWritable());
+					
+					try(Scanner scanner = new Scanner(System.in)){
+						while(true){
+							String msg = scanner.nextLine();
+							ByteBuf buffer = Unpooled.copiedBuffer(msg, Charset.defaultCharset());
+							future.channel().writeAndFlush(buffer);
+							if(msg.startsWith("done")){
+								break;
+							}
+						}
+					}
+				}
+				else{
+					Throwable cause = future.cause();
+					cause.printStackTrace();
+				}
+			}
+		});
+	}
+	
+}
