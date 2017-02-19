1dd8322ef4d598c0b666d4149722b6b8be75da43
Hoon Jang <lunawyrd@naver.com>
2017-02-19 20:33:27 +0900

    update Example4_1_basic, Example4_2_basic

--- projects/netty-in-action/src/main/java/kr/lunawyrd/nettypractice/lecture4/Example4_2_basic.java
+++ projects/netty-in-action/src/main/java/kr/lunawyrd/nettypractice/lecture4/Example4_2_basic.java
@@ -1,12 +1,15 @@
 package kr.lunawyrd.nettypractice.lecture4;
 
+import java.io.IOException;
+
 public class Example4_2_basic {
 
 	public static void main(String[] args) {
-
-		
-		
-		
-		
+		try {
+			PlainNioServer server = new PlainNioServer();
+			server.serve(7777);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
